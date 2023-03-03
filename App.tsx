0f6c217de85e6e299c5b3026dd3c42d321439592
Kevin Chambi <kchambit@unsa.edu.pe>
2023-03-03 10:50:43 -0500

    First commit (Project setup)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from "react-native";
+import colors from "./src/theme/colors";
+import fonts from "./src/theme/fonts";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ color: colors.primary, fontSize: fonts.size.xxlg }}>
+        Hello I'm croko22
+      </Text>
+      <Button title="Click me" onPress={() => alert("Hello")} />
+      <AntDesign name="home" size={30} color={colors.primary} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  primary: "#405de6",
+  white: "#FFF",
+  black: "#000",
+  gray: "#666666",
+  lightGray: "#cccccc",
+};
+
+export default colors;
